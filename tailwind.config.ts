21b839ac3d19b1bce5459dfa49c538bc5084a637
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-08-30 21:05:52 +0900

    feat: 초기세팅 tailwind 공통 색상 변수 추가 #25
    - black, white, gray, purple 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        black: '#101318',
+        white: '#FFF',
+        gray: {
+          100: '#F2F4F8',
+          300: '#CFDBEA',
+          500: '#9FACBD',
+          800: '#2D3034',
+        },
+        purple: {
+          100: '#6A42DB',
+          '10%': '#F1EDFC',
+        },
       },
     },
   },
