e1bbf1541bb04c077bb10b42aecb4070159cc5a1
Choi Young Sun <sunn7931@naver.com>
2024-08-31 22:22:30 +0900

    remove: 테스트용 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Image from 'next/image';
-import WineCard from '@/components/common/WineCard';
 
 export default function Home() {
-  return (
-    <>
-      <WineCard />
-      <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
-    </>
-  );
+  <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>;
 }
