0e5fb4e32efd4476011dcf2734a226dced4f7121
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-03 23:18:31 +0900

    fix: 불필요 파일 수정

--- api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// export async function fetchProductReviews(teamId: string, productId: string) {
-//   const response = await fetch(
-//     `https://mogazoa-api.vercel.app/test/products/${productId}/reviews`
-//   );
-//   if (!response.ok) {
-//     throw new Error('Failed to fetch reviews');
-//   }
-//   return response.json();
-// }
