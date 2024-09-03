fe2e3b025bc9f005fd287ffcbb6ed4925652635e
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-09-03 16:13:01 +0900

    remove : 헤더 test용 코드 삭제 #35

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import Header from '@/components/common/header';
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
   );
 }
