fa17aa9894a6e352310f2d3a1c418a050cb05f9b
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-09-03 16:14:18 +0900

    remove : 헤더 test용 import 코드 삭제 #35

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image';
-import Header from '@/components/common/header';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
