b54c849505d26776e0260e1c302d11802570f6f0
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-09-04 18:27:33 +0900

    feat: 목록 페이지 기본 페이지 구조 생성

---
+++ app/wines/page.tsx
@@ -0,0 +1,7 @@
+export default function Wines() {
+  return (
+    <div className="bg-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24" />
+    </div>
+  );
+}
---
+++ components/wines/WineList.tsx
@@ -0,0 +1,3 @@
+export default function WineList() {
+  return <div />;
+}
