3098192b1a9519fd7d5a68b48ee5448fbc8b2378
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-09-05 01:28:36 +0900

    design: 회원가입 페이지 UI 틀 수정 #56

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Header from '@/components/common/Header';
+// import Header from '@/components/common/Header';
+import Register from '@/app/register/Register';
 
 export default function Home() {
   return (
     <div className="bg-white">
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24" />
+      {/* <Header /> */}
+
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24">
+        <Register />
+      </main>
     </div>
   );
 }
