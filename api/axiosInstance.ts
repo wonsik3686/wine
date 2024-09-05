c2249ccf4fd446a5729e2b441177645e7abb3e10
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-05 16:57:43 +0900

    feat: 주요기능 axios interceptor 기본 틀 추가 #96

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,7 +1,41 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const BASE_URL = 'https://winereview-api.vercel.app';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    // const cookieStore = cookies();
+    // const accessToken = cookieStore.get(AUTH_COOKIE_KEYS.accessToken)?.value;
+    const accessToken = 'token';
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+      return config;
+    }
+    // throw new Error('로그인이 필요합니다.');
+    return config;
+  },
+  (error) => {
+    // 에러 핸들링
+    return Promise.reject(error);
+  }
+);
+
+// axiosInstance.interceptors.response.use(
+//   (res) => res,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       await axiosInstance.post('/auth/refresh-token', undefined);
+//       originalRequest._retry = true;
+//       return axiosInstance(originalRequest);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export { axiosInstance };
