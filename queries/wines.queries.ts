37a71142c19305b3155682affae31cd7c4c11941
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-05 23:19:05 +0900

    feat: 주요기능 controller  추가 #96

--- queries/wines.queries.ts
+++ queries/wines.queries.ts
@@ -1,10 +1,10 @@
-import { getWineDetail } from '@/api/wine.api';
+import { getWineDetail } from '@/api/wines.api';
 import { WineDetailRequest } from '@/types/dto/request/wine.request.types';
 import { WineDetailResponse } from '@/types/dto/response/wine.response.types';
 import { useQuery } from '@tanstack/react-query';
 
 export default function useWineDetail({ ...params }: WineDetailRequest) {
-  const { data, error, isLoading } = useQuery<WineDetailResponse>({
+  const { data, error, isLoading, refetch } = useQuery<WineDetailResponse>({
     queryKey: ['wineDetail', params.id],
     queryFn: async () => {
       const response = await getWineDetail(params);
@@ -16,5 +16,6 @@ export default function useWineDetail({ ...params }: WineDetailRequest) {
     wineDetail: data,
     wineDetailError: error,
     isWineDetailLoading: isLoading,
+    refetchWineDetail: refetch,
   };
 }
