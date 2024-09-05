7c8a5a4f93b6f8243440b97cb98cc06deafc9c10
gjrefa9139 <gjrefa9139@gmail.com>
2024-09-05 20:14:11 +0900

    fix: 공통레이아웃 경로설정 오류 수정

--- providers/RootHeader.tsx
+++ providers/RootHeader.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 
 function RootHeader({ children }: { children: React.ReactNode | JSX.Element }) {
   const pathname = usePathname();
-  const isAuthPage = pathname === ('/login' || '/signup');
+  const isAuthPage = pathname === '/login' || pathname === '/signup';
 
   return (
     <>
