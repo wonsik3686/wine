687b5f3db81c8df8626689b1e878a1a0a304b844
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-09-07 01:57:11 +0900

    feat: 컴포넌트 라디오 버튼 추가 #43

---
+++ components/common/RadioButton.tsx
@@ -0,0 +1,28 @@
+type RadioButtonProps = {
+  isChecked?: boolean;
+  onChange?: (checked: boolean) => void;
+};
+
+const RadioButton: React.FC<RadioButtonProps> = ({
+  isChecked = false,
+  onChange,
+}) => {
+  const handleClick = () => {
+    if (onChange) {
+      onChange(!isChecked); // 체크 상태 반전
+    }
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`inline-flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-100`}
+    >
+      {isChecked && (
+        <div className="h-[10px] w-[10px] rounded-[3px] bg-purple-100" />
+      )}
+    </div>
+  );
+};
+
+export default RadioButton;
