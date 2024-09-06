947e734eb2a0754031abda30f0bda14f3b733e36
KorpoQ <122213489+KorpoQ@users.noreply.github.com>
2024-09-07 02:16:16 +0900

    delete: 불필요한 파일 삭제 #38

--- app/wines/[wineId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function WinesDetail() {
-  return (
-    <div className="bg-white">
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24" />
-    </div>
-  );
-}
