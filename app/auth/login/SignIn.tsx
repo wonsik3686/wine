847553781bd8d3edb64e556ee6fa5be24575329a
ayoooyh <127219927+ayoooyh@users.noreply.github.com>
2024-09-06 23:55:57 +0900

    feat: 로그인 페이지 form 수정 #55

--- app/auth/login/SignIn.tsx
+++ app/auth/login/SignIn.tsx
@@ -2,75 +2,150 @@
 
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
+import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const [values, setValues] = useState<FormValues>({
+    email: '',
+    password: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+
+    // setFormErrors((prevErrors) => ({
+    //   ...prevErrors,
+    //   [name]: !value,
+    // }));
+  }, []);
+  const router = useRouter();
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { email, password } = values;
+
+    if (!email || !password) {
+      setFormErrors({
+        email: !email,
+        password: !password,
+      });
+      return;
+    }
+
+    await axios.post(
+      '/auth/signIn',
+      {
+        email,
+        password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    router.push('/');
+  }
+
   return (
     <>
       <Link href="/">
         <Image
+          className="mb-16"
           src="/icons/wineBlackLogo-l.svg"
           alt="로고"
           width={104}
           height={30}
         />
       </Link>
-      <>
+      <form onSubmit={handleSubmit}>
         <Input
           label="이메일"
-          errorMessage="이메일을 입력해주세요"
+          errorMessage={formErrors.email ? '이메일을 입력해주세요' : undefined}
           placeholder="이메일 입력"
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
         />
 
-        <Input
-          label="비밀번호"
-          errorMessage="비밀번호를 입력해주세요"
-          placeholder="비밀번호 입력"
-        />
-      </>
-      {/* Link href 재설정 필요, 임시로 "/" 페이지 설정 */}
-      <div>
-        <Link href="/">비밀번호를 잊으셨나요?</Link>
-      </div>
-      <div>
-        <Button
-          buttonStyle="box"
-          buttonWidth="fitToParent"
-          buttonColor="purple"
-          textColor="white"
-        >
-          로그인
-        </Button>
-        <Button
-          buttonStyle="box"
-          buttonWidth="fitToParent"
-          buttonColor="white"
-          textColor="black"
-        >
-          <Image
-            src="/icons/iconGoogle.svg"
-            alt="google"
-            width={24}
-            height={24}
+        <div className="pt-[25px]">
+          <Input
+            label="비밀번호"
+            errorMessage={
+              formErrors.password ? '비밀번호를 입력해주세요' : undefined
+            }
+            placeholder="비밀번호 입력"
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
           />
-          Google로 시작하기
-        </Button>
-        <Button
-          buttonStyle="box"
-          buttonWidth="fitToParent"
-          buttonColor="white"
-          textColor="black"
-        >
-          <Image
-            src="/icons/iconKakaotalk.svg"
-            alt="kakao"
-            width={24}
-            height={24}
-          />
-          kakao로 시작하기
-        </Button>
-      </div>
+        </div>
+
+        <div className="pt-[10px] text-[14px] text-purple-700">
+          {/* Link href 재설정 필요, 임시로 "/" 페이지 설정 */}
+          <Link href="/">비밀번호를 잊으셨나요?</Link>
+        </div>
+
+        <div className=" mb-[15px] mt-[56px]">
+          <Button
+            buttonStyle="box"
+            buttonWidth="fitToParent"
+            buttonColor="purple"
+            textColor="white"
+            type="submit"
+          >
+            로그인
+          </Button>
+
+          <Button
+            buttonStyle="box"
+            buttonWidth="fitToParent"
+            buttonColor="white"
+            textColor="black"
+          >
+            <Image
+              src="/icons/iconGoogle.svg"
+              alt="google"
+              width={24}
+              height={24}
+            />
+            Google로 시작하기
+          </Button>
+
+          <Button
+            buttonStyle="box"
+            buttonWidth="fitToParent"
+            buttonColor="white"
+            textColor="black"
+          >
+            <Image
+              src="/icons/iconKakaotalk.svg"
+              alt="kakao"
+              width={24}
+              height={24}
+            />
+            kakao로 시작하기
+          </Button>
+        </div>
+      </form>
       <div>
         계정이 없으신가요?
         {/* Link href 재설정 필요, 임시로 "/" 페이지 설정 */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import SignIn from './auth/login/SignIn';
 
 export default function Home() {
   return (
+    // <BrowserRouter>
+    //   <div className="bg-white">
+    //     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24">
+    //       <Routes>
+    //         <Route path="signIn" element={<SignIn />} />
+    //       </Routes>
+    //     </main>
+    //   </div>
+    // </BrowserRouter>
+
     <div className="bg-white">
       <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24">
         <SignIn />
