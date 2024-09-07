5c71de47aac11bb2a0db6e9a10bc0002b0b7c405
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-07 15:27:51 +0900

    fix: 주요기능 와인상세별점 WineReviewsRating 컴포넌트 데이터 연동 #21

--- components/wines/WineReviewsRating.tsx
+++ components/wines/WineReviewsRating.tsx
@@ -12,18 +12,31 @@ type WineReviewsRatingProps = Pick<
 
 export default function WineReviewsRating({
   avgRatings,
+  reviewCount,
+  avgRating,
 }: WineReviewsRatingProps) {
+  // Ratings에 넣기 위한 변환
+  const ratingsArray = Object.entries(avgRatings).map(([key, value]) => ({
+    key: Number(key),
+    value: value,
+  }));
   return (
     <>
       <div className="mt-14 flex pc:flex-col tab:justify-center mob:mt-10 mob:flex-col">
         <div className="flex tab:flex-col mob:mb-4 mob:flex-row mob:justify-between">
           <div className="flex justify-center pc:mb-5 tab:mb-5 tab:mr-20 mob:mb-0 mob:mr-0">
             <b className="font-font-sans text-7xl font-extrabold text-gray-800">
-              4.8
+              {avgRating.toFixed(1)}
             </b>
             <div className="ml-5 flex flex-col justify-center">
-              <StarRating isInteractive={false} rating={4.5} size="small" />
-              <p className="mt-[0.31rem] text-gray-500">5,446개의 후기</p>
+              <StarRating
+                isInteractive={false}
+                rating={avgRating}
+                size="small"
+              />
+              <p className="mt-[0.31rem] text-gray-500">
+                {reviewCount.toLocaleString()}개의 후기
+              </p>
             </div>
           </div>
           <div className="pc:hidden">
@@ -38,16 +51,7 @@ export default function WineReviewsRating({
           </div>
         </div>
         <div className="pc: mb-[1.88rem]">
-          <RatingProgressbar
-            Ratings={[
-              { key: 5, value: 700 }, // 5점의 개수는 50개
-              { key: 4, value: 2200 }, // 4점의 개수는 30개
-              { key: 3, value: 1300 }, // 3점의 개수는 20개
-              { key: 2, value: 446 }, // 2점의 개수는 10개
-              { key: 1, value: 300 }, // 1점의 개수는 5개
-            ]}
-            totalCount={5446}
-          />
+          <RatingProgressbar Ratings={ratingsArray} totalCount={reviewCount} />
         </div>
         <div className="tab:hidden">
           <Button
