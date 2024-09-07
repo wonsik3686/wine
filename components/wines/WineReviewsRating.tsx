7acf1be9074cba1ff1d961ae753523826c88c049
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-07 15:34:42 +0900

    fix: 와인상세별점 eslint 오류 수정 #21

--- components/wines/WineReviewsRating.tsx
+++ components/wines/WineReviewsRating.tsx
@@ -18,42 +18,23 @@ export default function WineReviewsRating({
   // Ratings에 넣기 위한 변환
   const ratingsArray = Object.entries(avgRatings).map(([key, value]) => ({
     key: Number(key),
-    value: value,
+    value,
   }));
   return (
-    <>
-      <div className="mt-14 flex pc:flex-col tab:justify-center mob:mt-10 mob:flex-col">
-        <div className="flex tab:flex-col mob:mb-4 mob:flex-row mob:justify-between">
-          <div className="flex justify-center pc:mb-5 tab:mb-5 tab:mr-20 mob:mb-0 mob:mr-0">
-            <b className="font-font-sans text-7xl font-extrabold text-gray-800">
-              {avgRating.toFixed(1)}
-            </b>
-            <div className="ml-5 flex flex-col justify-center">
-              <StarRating
-                isInteractive={false}
-                rating={avgRating}
-                size="small"
-              />
-              <p className="mt-[0.31rem] text-gray-500">
-                {reviewCount.toLocaleString()}개의 후기
-              </p>
-            </div>
-          </div>
-          <div className="pc:hidden">
-            <Button
-              buttonColor="purple"
-              buttonStyle="box"
-              buttonWidth="fitToChildren"
-              textColor="white"
-            >
-              리뷰 남기기
-            </Button>
+    <div className="mt-14 flex pc:flex-col tab:justify-center mob:mt-10 mob:flex-col">
+      <div className="flex tab:flex-col mob:mb-4 mob:flex-row mob:justify-between">
+        <div className="flex justify-center pc:mb-5 tab:mb-5 tab:mr-20 mob:mb-0 mob:mr-0">
+          <b className="font-font-sans text-7xl font-extrabold text-gray-800">
+            {avgRating.toFixed(1)}
+          </b>
+          <div className="ml-5 flex flex-col justify-center">
+            <StarRating isInteractive={false} rating={avgRating} size="small" />
+            <p className="mt-[0.31rem] text-gray-500">
+              {reviewCount.toLocaleString()}개의 후기
+            </p>
           </div>
         </div>
-        <div className="pc: mb-[1.88rem]">
-          <RatingProgressbar Ratings={ratingsArray} totalCount={reviewCount} />
-        </div>
-        <div className="tab:hidden">
+        <div className="pc:hidden">
           <Button
             buttonColor="purple"
             buttonStyle="box"
@@ -64,6 +45,19 @@ export default function WineReviewsRating({
           </Button>
         </div>
       </div>
-    </>
+      <div className="pc: mb-[1.88rem]">
+        <RatingProgressbar Ratings={ratingsArray} totalCount={reviewCount} />
+      </div>
+      <div className="tab:hidden">
+        <Button
+          buttonColor="purple"
+          buttonStyle="box"
+          buttonWidth="fitToChildren"
+          textColor="white"
+        >
+          리뷰 남기기
+        </Button>
+      </div>
+    </div>
   );
 }
