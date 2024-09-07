92bd45f314dad486e2d28dc06ca27c8352711c30
ayoooyh <syh9566@naver.com>
2024-09-08 03:24:13 +0900

    remove: 로그인 페이지 불필요한 코드 삭제 #55

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,6 @@ import Header from '@/components/common/Header';
 
 export default function Home() {
   return (
-    // <BrowserRouter>
-    //   <div className="bg-white">
-    //     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24">
-    //       <Routes>
-    //         <Route path="signIn" element={<SignIn />} />
-    //       </Routes>
-    //     </main>
-    //   </div>
-    // </BrowserRouter>
-
     <div className="bg-white">
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-center bg-white p-24" />
