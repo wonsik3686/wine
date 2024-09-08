c8ca02e1e269e259da946cba857f1991c9cafea7
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-09-08 18:20:30 +0900

    fix: RadioButton ESLint 수정 #38

--- components/common/RadioButton.tsx
+++ components/common/RadioButton.tsx
@@ -1,12 +1,12 @@
-type RadioButtonProps = {
-  isChecked?: boolean;
-  onChange?: (checked: boolean) => void;
-};
+'use client';
 
-const RadioButton: React.FC<RadioButtonProps> = ({
+export default function RadioButton({
   isChecked = false,
   onChange,
-}) => {
+}: {
+  isChecked?: boolean;
+  onChange?: (checked: boolean) => void;
+}) {
   const handleClick = () => {
     if (onChange) {
       onChange(!isChecked); // 체크 상태 반전
@@ -16,13 +16,11 @@ const RadioButton: React.FC<RadioButtonProps> = ({
   return (
     <div
       onClick={handleClick}
-      className={`inline-flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-100`}
+      className="inline-flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-100"
     >
       {isChecked && (
         <div className="h-[10px] w-[10px] rounded-[3px] bg-purple-100" />
       )}
     </div>
   );
-};
-
-export default RadioButton;
+}
