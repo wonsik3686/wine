9ddfcd6375ea1c252c685cd36bc40443d0ca4bc7
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-09-10 10:32:52 +0900

    feat: 와인 추천 및 와인 목록 API 완성 #38 #42

--- api/wines.api.ts
+++ api/wines.api.ts
@@ -1,6 +1,15 @@
-import { WineDetailRequest } from '@/types/dto/request/wine.request.types';
-import { WineDetailResponse } from '@/types/dto/response/wine.response.types';
+import {
+  WineDetailRequest,
+  WineListRequest,
+} from '@/types/dto/request/wine.request.types';
+import {
+  RecommendedWineResponse,
+  WineDetailResponse,
+  WineListResponse,
+} from '@/types/dto/response/wine.response.types';
 import { axiosInstance } from './_axiosInstance';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const getWineDetail = async ({ ...params }: WineDetailRequest) => {
   const response = await axiosInstance<WineDetailResponse>({
@@ -9,4 +18,22 @@ export const getWineDetail = async ({ ...params }: WineDetailRequest) => {
     params,
   });
   return response;
+};
+
+export const getWine = async ({ ...params }: WineListRequest) => {
+  const response = await axiosInstance<WineListResponse>({
+    method: 'GET',
+    url: `${API_BASE_URL}/wines`,
+    params,
+  });
+  return response.data;
+};
+
+export const getRecommendedWines = async (limit: number) => {
+  const response = await axiosInstance<RecommendedWineResponse>({
+    method: 'GET',
+    url: `${API_BASE_URL}/wines/recommended`,
+    params: { limit },
+  });
+  return response.data;
 };
