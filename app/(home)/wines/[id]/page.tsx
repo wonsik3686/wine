00570ac0268bcf7551249d14e75c5319da891cee
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-12 01:18:09 +0900

    feat: 주요기능 와인상세페이지 NoReviewContent 추가 #109

--- app/(home)/wines/[id]/page.tsx
+++ app/(home)/wines/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import AddReviewModal from '@/components/modal/reviewmodal/AddReviewModal';
+import NoReviewContent from '@/components/wines/NoReviewContent';
 import WineDetailCard from '@/components/wines/WineDetailCard';
 import WineReviewList from '@/components/wines/WineReviewList';
 import WineReviewsRating from '@/components/wines/WineReviewsRating';
@@ -15,22 +16,23 @@ export default function WineDetail({ params }: { params: { id: number } }) {
 
   return (
     <>
-      <section className="hidden">{params.id}</section>
       {wineDetail && <WineDetailCard wineDetail={wineDetail} />}
       <section className="flex tab:flex-col-reverse tab:gap-[2.25rem] mob:gap-[1.25rem] pc:flex-row pc:gap-[3.75rem] ">
-        {wineDetail && (
-          <WineReviewList
-            reviews={wineDetail.reviews}
-            onOpenReviewModal={handleOpenAddReview}
-          />
-        )}
-        {wineDetail && (
-          <WineReviewsRating
-            avgRating={wineDetail.avgRating}
-            avgRatings={wineDetail.avgRatings}
-            reviewCount={wineDetail.reviewCount}
-            onOpenReviewModal={handleOpenAddReview}
-          />
+        {wineDetail?.reviews && wineDetail.reviews.length > 0 ? (
+          <>
+            <WineReviewList
+              reviews={wineDetail.reviews}
+              onOpenReviewModal={handleOpenAddReview}
+            />
+            <WineReviewsRating
+              avgRating={wineDetail.avgRating}
+              avgRatings={wineDetail.avgRatings}
+              reviewCount={wineDetail.reviewCount}
+              onOpenReviewModal={handleOpenAddReview}
+            />
+          </>
+        ) : (
+          <NoReviewContent />
         )}
       </section>
       {wineDetail && (
