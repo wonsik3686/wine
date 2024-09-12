caec69b28523ced499733ba832b6930ba06c5592
gjrefa9139 <gjrefa9139@gmail.com>
2024-09-12 16:13:49 +0900

    refactor: 유저타입 세분화

--- types/user.types.ts
+++ types/user.types.ts
@@ -7,3 +7,102 @@ export type User = {
   nickname: string; // 닉네임, 최소 길이 1, 최대 길이 30
   id: number; // 최소값 1
 };
+
+export type Login = {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type Params =
+  | {
+      limit: number;
+      cursor?: number | null;
+    }
+  | string;
+
+type Aroma = [
+  'CHERRY',
+  'BERRY',
+  'OAK',
+  'VANILLA',
+  'PEPPER',
+  'BAKING',
+  'GRASS',
+  'APPLE',
+  'PEACH',
+  'CITRUS',
+  'TROPICAL',
+  'MINERAL',
+  'FLOWER',
+  'TOBACCO',
+  'EARTH',
+  'CHOCOLATE',
+  'SPICE',
+  'CARAMEL',
+  'LEATHER',
+];
+
+export type Reviews = {
+  list: {
+    id: number;
+    rating: number;
+    lightBold: number;
+    smoothTannic: number;
+    drySweet: number;
+    softAcidic: number;
+    aroma: Aroma;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    user: {
+      id: number;
+      nickname: string;
+      image: string;
+    };
+    wine: {
+      id: number;
+      name: string;
+      region: string;
+      image: string;
+      price: number;
+      avgRating: number;
+      type: string;
+    };
+  }[];
+  totalCount: number;
+  nextCursor: number | null;
+};
+
+export type WineList = {
+  id: number;
+  name: string;
+  region: string;
+  image: string;
+  price: number;
+  type: 'RED' | 'WHITE' | 'SPARKLING';
+  avgRating: number;
+  reviewCount: number;
+  recentReview:
+    | {
+        user: {
+          id: number;
+          nickname: string;
+          image: string | null;
+        };
+        updatedAt: string;
+        createdAt: string;
+        content: string;
+        aroma: Aroma;
+        rating: number;
+        id: number;
+      }[]
+    | null;
+  userId: number;
+};
+
+export type Wines = {
+  list: WineList[];
+  totalCount: number;
+  nextCursor: number | null;
+};
