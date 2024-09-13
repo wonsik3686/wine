58845ae95687e249615af4ea5428e9917013a30a
gjrefa9139 <gjrefa9139@gmail.com>
2024-09-13 15:48:27 +0900

    refactor: 리액트 hook에서 useUserStore로 변경

--- app/(home)/myprofile/page.tsx
+++ app/(home)/myprofile/page.tsx
@@ -1,11 +1,10 @@
-'use client';
-
+import Profile from '@/components/auth/Profile';
 import UserTapMenu from '@/components/auth/UserTapMenu';
 
 function MyProfile() {
   return (
     <main className="m-auto box-content flex max-w-[71.25rem] gap-x-[3.75rem] gap-y-[1.875rem] p-5 tab:flex-col mob:px-4 tab2:gap-y-10 pc:py-10">
-      {/* <Profile className="pc:self-start" user={user} setUser={setUser} /> */}
+      <Profile />
 
       <UserTapMenu />
     </main>
