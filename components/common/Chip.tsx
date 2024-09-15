18c662a8ffc7bcd3cb0ba5e2050e56f0f1a33519
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-09-15 22:30:37 +0900

    fix: Chip ring 삭제 및 border 추가 #147

--- components/common/Chip.tsx
+++ components/common/Chip.tsx
@@ -67,12 +67,15 @@ function Chip({
 }: ChipProps): JSX.Element {
   return (
     <button
-      className={clsx('rounded-full px-4 py-2 text-base font-medium ring-1', {
-        'bg-purple-600 text-white ring-purple-100': selected,
-        'bg-white text-inherit ring-gray-300': !selected,
-        'cursor-pointer': !isDisabled,
-        'cursor-default': isDisabled,
-      })}
+      className={clsx(
+        'rounded-full border-[1px] px-4 py-2 text-base font-medium',
+        {
+          'border-purple-100 bg-purple-600  text-white': selected,
+          'border-gray-300 bg-white  text-inherit': !selected,
+          'cursor-pointer': !isDisabled,
+          'cursor-default': isDisabled,
+        }
+      )}
       onClick={!isDisabled ? onClick : undefined}
       type="button"
     >
