23e33746e80b88e8f20939388992b85608d4df27
nulllife2510 <122213489+nulllife2510@users.noreply.github.com>
2024-09-16 19:09:29 +0900

    feat: 검색한 와인이 없을 경우, '등록된 와인이 없어요' #167

--- components/wines/WineCardGallery.tsx
+++ components/wines/WineCardGallery.tsx
@@ -6,6 +6,43 @@ type WineCardGalleryProps = {
 };
 
 export default function WineCardGallery({ wineList }: WineCardGalleryProps) {
+  if (wineList.length === 0) {
+    return (
+      <div
+        className="
+          flex h-full w-full flex-col
+          items-center justify-center
+          px-4 py-[30px]
+          tab:py-[15px] mob:py-[0px]
+        "
+      >
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div
+            className="
+              flex h-[200px] w-[200px]
+              items-center justify-center
+              rounded-full bg-gray-100
+              bg-contain bg-center
+              bg-no-repeat tab:h-[160px]
+              tab:w-[160px] mob:h-[120px] mob:w-[120px]
+            "
+            style={{ backgroundImage: 'url("/icons/iconNoContent.svg")' }}
+          />
+          <div
+            className="
+              text-center text-[24px]
+              font-normal leading-relaxed text-gray-500
+              tab:text-[20px]
+              mob:text-[16px]
+            "
+          >
+            등록된 와인이 없어요
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       {wineList.map((wine) => (
